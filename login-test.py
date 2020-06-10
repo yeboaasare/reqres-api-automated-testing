d4facaa650924a6d58df8b5ae1bbb8f8906f2fdd
tonyyeboah <anasare3.cos@gmail.com>
2020-06-10 07:51:39 +0000

    reformated code

--- login-test.py
+++ login-test.py
@@ -3,19 +3,17 @@ import requests
 
 
 def test_successful_login(supply_url):
-    url = supply_url['url'] + constants.LOGIN
+    url = supply_url["url"] + constants.LOGIN
     data = {
-        'email': 'eve.holt@reqres.in',
-        'password': 'cityslicka',
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka",
     }
     response = requests.post(url, data=data)
     assert response.status_code == constants.STATUS_OK
 
 
 def test_unsuccessful_login(supply_url):
-    url = supply_url['url'] + constants.LOGIN
-    data = {
-        'email' : 'peter@klaven'
-    }
+    url = supply_url["url"] + constants.LOGIN
+    data = {"email": "peter@klaven"}
     response = requests.post(url, data=data)
     assert response.status_code == constants.STATUS_BAD_REQUEST
--- register-test.py
+++ register-test.py
@@ -4,22 +4,21 @@ import json
 
 
 def test_successful_registration(supply_url):
-	url = supply_url['url'] + constants.REGISTER
-	data = {
-		'email': 'eve.holt@reqres.in',
-		'password': 'pistol',
-	}
-	response = requests.post(url, data=data)
-	token = json.loads(response.text)
-	assert response.status_code == constants.STATUS_OK
-	assert token['token'] == 'QpwL5tke4Pnpja7X4'
+    url = supply_url["url"] + constants.REGISTER
+    data = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol",
+    }
+    response = requests.post(url, data=data)
+    token = json.loads(response.text)
+    assert response.status_code == constants.STATUS_OK
+    assert token["token"] == "QpwL5tke4Pnpja7X4"
 
 
 def test_unsuccessful_registration(supply_url):
-	url = supply_url['url'] + constants.REGISTER
-	data = {
-		'email': 'sydney@fife',
-	}
-	response = requests.post(url, data=data)
-	assert response.status_code == constants.STATUS_BAD_REQUEST
-
+    url = supply_url["url"] + constants.REGISTER
+    data = {
+        "email": "sydney@fife",
+    }
+    response = requests.post(url, data=data)
+    assert response.status_code == constants.STATUS_BAD_REQUEST
